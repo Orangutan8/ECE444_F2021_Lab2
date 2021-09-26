9d7f35433374d6aa8e4564b0ba10a8ca57d37b13
ian-sartor <ian.sartor@mail.utoronto.ca>
2021-09-26 16:49:43 -0400

    Probably correct to Activity 4.1

---
+++ P4.1.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, flash, redirect, url_for, session
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(Form):    
+    name = StringField('What is your name?', validators=[DataRequired()])    
+    submit = SubmitField('Submit')
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'I am a hacker now :)'
+bootstrap=Bootstrap(app)
+moment=Moment(app)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', current_time=datetime.utcnow(), form = form, name = session.get('name'))
+    #return render_template('index.html', current_time=datetime.utcnow())
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
+
+if __name__=='__main__':
+    app.run(debug=True)
